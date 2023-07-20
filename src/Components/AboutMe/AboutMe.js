977b6b9a1fe6497e3bf6707f2cbcd60efebdeadc
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-07-20 14:40:48 -0400

    Add routes, logo nad intital build out of all componetns

---
+++ src/Components/AboutMe/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <div>
+      <h1>AboutMe</h1>
+      <p>This is the AboutMe component.</p>
+    </div>
+  );
+}
+
+export default AboutMe;
---
+++ src/Components/Photography/Photography.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Photography() {
+  // Component code here
+}
+
+export default Photography;
