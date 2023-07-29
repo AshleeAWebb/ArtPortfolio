fedbfe280abb939c88a53d69879e19d1ea058620
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-07-29 16:30:53 -0400

    Add images

--- src/Components/FineArt/FineArt.js
+++ src/Components/FineArt/FineArt.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 function FineArt() {
-  // Component code here
+  return (
+    <div>
+      <img
+        src="https://storage.cloud.google.com/ashaubree-images/quickstart-folder/Float.jpg?authuser=4"
+        alt="Fine Art"
+        style={{ width: '300px', height: 'auto' }}
+      />
+
+<img
+        src="https://i.imgur.com/3WMQzSe.jpg"
+        alt="Fine Art"
+        style={{ width: '300px', height: 'auto' }}
+      />
+    </div>
+  );
 }
 
 export default FineArt;
