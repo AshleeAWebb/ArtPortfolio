c9aef926b1d9c2e1b71f21f5f454dc48498c48b4
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-07-29 20:40:04 -0400

    Add page loader

--- src/Components/ReusableComponents/PageLoader/PageLoader.js
+++ src/Components/ReusableComponents/PageLoader/PageLoader.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './PageLoader.css';
-import spooky from '../../../Assets/SpookyCamera.png';
 
 function PageLoader() {
   return (
-    <div className="page-loader-container">
-    <div className="spooky-loader">
-      <div className='spooky'><img src={spooky} alt="spooky logo" /></div>
-      <div className='spooky'><img src={spooky} alt="spooky logo" /></div>
-      <div className='spooky'><img src={spooky} alt="spooky logo" /></div>
-    </div>
+    <div className="loading-screen">
+      <div className="camera-lens">
+        <div className="spinning">
+          <div className="circle" aria-label="Loading"></div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -5,13 +5,13 @@ import spookyLogo from '../../Assets/Spooky Logo.svg';
 const Welcome = () => {
   return (
     <div className="welcome-container">
-      <h1 className="title">Welcome to My Creative Wonderland! 🎨🌐</h1>
-      <p>I'm Ash, an artist and web developer on a journey to bring imagination to life.<br></br>
+      <h1 className="welcome-title">Welcome to My Creative World! 🎨🌐</h1>
+      <p className='welcomepage-text'>I'm Ash, an artist and web developer on a journey to bring imagination to life.<br></br>
 Whether I'm wielding a paintbrush or typing lines of code, I'm always thrilled to create something extraordinary.<br></br>
 Feel free to take a look around and explore the vibrant world of colors, shapes, and digital wonders.<br></br>
 </p> 
-<p>
-<h3>Have a project in mind?</h3>
+<h3 className='welcomepage-break'>Have a project in mind?</h3>
+<p className='welcomepage-text'>
  Let's team up and turn your vision into reality! Whether it's a stunning artwork that speaks to the 
 heart or a cutting-edge website that leaves a lasting impression, I'm here to make it happen.<br></br>
 <br></br>
