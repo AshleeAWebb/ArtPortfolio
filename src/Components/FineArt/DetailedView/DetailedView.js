9b2b44c37e7467e2334701d517520db12fb17772
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-08-02 13:09:09 -0400

    add zoom image feature

--- src/Components/FineArt/DetailedView/DetailedView.js
+++ src/Components/FineArt/DetailedView/DetailedView.js
@@ -1,4 +1,5 @@
-import React from 'react';
+// DetailedView.js
+import React, { useState } from 'react';
 import { useParams } from "react-router-dom";
 import fineArtData from '../../data/fineArtData';
 import '../DetailedView/DetailedView.css';
@@ -6,11 +7,24 @@ import '../DetailedView/DetailedView.css';
 function DetailedView() {
   const { id } = useParams();
   const artwork = fineArtData.find((art) => art.id === Number(id));
+  const [zoom, setZoom] = useState(1);
+
+  const handleZoomIn = () => setZoom(zoom * 1.1);
+  const handleZoomOut = () => setZoom(zoom / 1.1);
 
   return (
     <div className="detailed-container">
       <div className="image-container">
-        <img src={artwork.src} alt={artwork.alt} className="detailed-image" />
+        <img
+          src={artwork.src}
+          alt={artwork.alt}
+          className="detailed-image"
+          style={{ transform: `scale(${zoom})` }}
+        />
+        <div className="zoom-controls">
+          <button onClick={handleZoomIn}>➕</button> 
+          <button onClick={handleZoomOut}>➖</button> 
+        </div>
       </div>
       <div className="info-container">
         <h2>{artwork.title}</h2>
