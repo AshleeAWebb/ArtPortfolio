e21dc3e6aa40f289bd3edafee1143ed99a44763b
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-08-02 12:44:46 -0400

    Update photo page

--- src/Components/Photography/Photography.js
+++ src/Components/Photography/Photography.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import '../Photography/Photography.css';
+import albumData from '../data/albumData';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function Photography() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
+    <div className="photography-container">
       <h1>Photography Page</h1>
-      <p>Coming Soon...</p>
+      <div className="albums-container">
+        {albumData.map((album) => (
+          <Link to={`/album/${album.id}`} key={album.id}>
+            <img src={album.coverImage} alt={album.title} className="album-cover" />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
 
+
 export default Photography;
+
