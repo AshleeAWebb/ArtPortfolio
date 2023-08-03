87b9dff2c9500d3aced6e94a2bd90aebd322c6bf
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-08-03 13:34:13 -0400

    Update contact page and header for mobile

--- src/Components/ReusableComponents/Header/Header.js
+++ src/Components/ReusableComponents/Header/Header.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
 import logo from '../../../Assets/SpookyLogo.png';
 
 function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const handleHamburgerClick = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
   return (
     <header className='main-header'>
-           <NavLink className="home-nav-link" to="/">
-        <img className='logo' src={logo} alt="Logo" />
-      </NavLink>
-      <nav>
-            <NavLink className="nav-link" to="/fineart">Fine Art</NavLink>
-            <NavLink className="nav-link" to="/photography">Photography</NavLink>
-            <NavLink className="nav-link" to="/webdesign">Web Design</NavLink>
-            <NavLink className="nav-link" to="/about">About</NavLink>
-            <NavLink className="nav-link" to="/contact">Contact</NavLink>
+      <div className="header-content"> {/* Added this container */}
+        <NavLink className="home-nav-link" to="/">
+          <img className='logo' src={logo} alt="Logo" />
+        </NavLink>
+        <div className="hamburger" onClick={handleHamburgerClick}>
+          &#9776;
+        </div>
+      </div>
+      <nav className="nav">
+      <NavLink className="nav-link" to="/fineart">Fine Art</NavLink>
+          <NavLink className="nav-link" to="/photography">Photography</NavLink>
+          <NavLink className="nav-link" to="/webdesign">Web Design</NavLink>
+          <NavLink className="nav-link" to="/about">About</NavLink>
+          <NavLink className="nav-link" to="/contact">Contact</NavLink>
       </nav>
+      {mobileMenuOpen && (
+        <nav className="nav-mobile">
+          <NavLink className="nav-link" to="/fineart">Fine Art</NavLink>
+          <NavLink className="nav-link" to="/photography">Photography</NavLink>
+          <NavLink className="nav-link" to="/webdesign">Web Design</NavLink>
+          <NavLink className="nav-link" to="/about">About</NavLink>
+          <NavLink className="nav-link" to="/contact">Contact</NavLink>
+        </nav>
+      )}
     </header>
   );
 }
